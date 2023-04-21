6a6fa15438ba49c90b10ce30f119c4eac274170e
antialopez <antialopezgarcia@gmail.com>
2023-04-21 19:37:00 +0200

    call to useCase to repository

--- app/src/main/java/com/example/examplemvvm/ui/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/example/examplemvvm/ui/viewmodel/QuoteViewModel.kt
@@ -2,14 +2,38 @@ package com.example.examplemvvm.ui.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.examplemvvm.data.model.QuoteModel
 import com.example.examplemvvm.data.model.QuoteProvider
+import com.example.examplemvvm.domain.GetQuotesUseCase
+import kotlinx.coroutines.launch
 
 class QuoteViewModel :ViewModel(){
+    //quien pone las citas en la interfaz del usuario
     val quoteModel = MutableLiveData<QuoteModel>()
-    //Esta funcion será llamada por la activity
-    fun randomQuote(){
-        val currentQuote: QuoteModel = QuoteProvider.random()
-        quoteModel.postValue((currentQuote))
+    var getQuotesUseCase = GetQuotesUseCase()
+
+    fun onCreate() {
+        viewModelScope.launch {
+            val result: List<QuoteModel>? = getQuotesUseCase()
+            if(!result.isNullOrEmpty()){
+                //Esto es para iniciar la aplicación
+                quoteModel.postValue(result[0])
+            }
+        }
     }
+
+     /*
+    result llamara a GetQuoteUseCase
+    GetQuoteUseCase llama a QuoteRespository
+    La primera vez que se llame al QuoteRepository -> va a llamar al QuoteService
+    QuoteService -> hara una llamada al ApiClient a Retrofit que va a devolver ese listado de quotes y se los devuelve al QuoteService
+    Quote Service ->se los devuelve al QuoteRespository
+    */
+    fun randomQuote(){
+//        val currentQuote: QuoteModel = QuoteProvider.random()
+//        quoteModel.postValue((currentQuote))
+    }
+
+
 }
