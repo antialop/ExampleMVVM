e253f0da2dda07daa3ac6fa0a1e55d2cf9b986e9
antialopez <antialopezgarcia@gmail.com>
2023-04-21 19:35:10 +0200

    fix error

--- app/src/main/java/com/example/examplemvvm/data/model/QuoteModel.kt
+++ app/src/main/java/com/example/examplemvvm/data/model/QuoteModel.kt
@@ -2,4 +2,7 @@ package com.example.examplemvvm.data.model
 
 import com.google.gson.annotations.SerializedName
 
-data class QuoteModel(@SerializedName("quote ") val quote: String,@SerializedName("quote ") val author: String)
+data class QuoteModel(
+    @SerializedName("quote") val quote: String,
+    @SerializedName("author") val author: String
+    )
