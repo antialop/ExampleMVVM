7b57bfc793595846e0e5235e768181ed635453d8
antialopez <antialopezgarcia@gmail.com>
2023-04-24 13:53:16 +0200

    fix graddle

--- app/src/main/java/com/example/examplemvvm/core/RetrofitHelper.kt
+++ app/src/main/java/com/example/examplemvvm/core/RetrofitHelper.kt
@@ -4,5 +4,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-
+    fun proviteRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://drawsomething-59328-default-rtdb.europe-west1.firebasedatabase.app/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
